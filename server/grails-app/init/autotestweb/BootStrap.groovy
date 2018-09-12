8006d011864c270ca72a47ee3f1687ee08b1f152
461831455@qq.com <461831455@qq.com>
2018-09-13 01:12:09 +0800

    update

--- server/grails-app/init/autotestweb/BootStrap.groovy
+++ server/grails-app/init/autotestweb/BootStrap.groovy
@@ -1,12 +1,19 @@
 package autotestweb
 
+import autotestweb.auth.RoleInfo
+import autotestweb.auth.UserInfo
+
 class BootStrap {
 
     def init = { servletContext ->
-        final String BACK_ADMIN='admin'
-        if(!User.findByUsername(BACK_ADMIN)){
-            new User(username: BACK_ADMIN,password: BACK_ADMIN).save()
-        }
+        //创建角色
+        new RoleInfo(authority: "ROLE_ADMIN", remark: "管理员")
+                .addToUserInfo(new UserInfo(username: "admin", password: "admin")).save()
+        new RoleInfo(authority: "ROLE_SUPSYS", remark: "超级管理员")
+                .addToUserInfo(new UserInfo(username: "super", password: "super")).save()
+        new RoleInfo(authority: "ROLE_USER", remark: "普通用户")
+                .addToUserInfo( new UserInfo(username: "user", password: "user")).save()
+
     }
     def destroy = {
     }
